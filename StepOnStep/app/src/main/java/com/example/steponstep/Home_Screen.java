fa909b61ed2d5ed9038e4b42d433b6934270af6f
muhamadadnanjassim <hamod2007adnan@gmail.com>
2022-09-01 15:15:35 +0300

    .

--- StepOnStep/app/src/main/java/com/example/steponstep/Home_Screen.java
+++ StepOnStep/app/src/main/java/com/example/steponstep/Home_Screen.java
@@ -8,42 +8,20 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.Viewport;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-
-public class Home extends AppCompatActivity {
+public class Home_Screen extends AppCompatActivity {
 
     TextView txt_currentAccel, txt_prevAccel, txt_acceleration;
-    ProgressBar prog_ShakeMeter;
-    Button B1;
+    ProgressBar prog_shakeMeter;
 
-    //define the sensor variables
+    // define the sensor variables
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
 
-
     private double accelerationCurrentValue;
     private double accelerationPreviousValue;
-
-    private int pointsPlotted = 5;
-    private int graphIntervalCounter = 0;
-
-    private Viewport viewport;
-
-    LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-            new DataPoint(0, 1),
-            new DataPoint(1, 5),
-            new DataPoint(2, 3),
-            new DataPoint(3, 2),
-            new DataPoint(4, 6)
-    });
-
 
     private SensorEventListener sensorEventListener = new SensorEventListener() {
         @Override
@@ -52,42 +30,32 @@ public class Home extends AppCompatActivity {
             float y = sensorEvent.values[1];
             float z = sensorEvent.values[2];
 
-            accelerationCurrentValue = Math.sqrt((x * x + y * y + z * z));
-            double changeInAccelleration = Math.abs(accelerationCurrentValue - accelerationPreviousValue);
+            accelerationCurrentValue = Math.sqrt( (x * x + y * y + z * z));
+            double changeInAcceleration = Math.abs(accelerationCurrentValue - accelerationPreviousValue);
             accelerationPreviousValue = accelerationCurrentValue;
 
-            //update text
+            //update text views
             txt_currentAccel.setText("Current = " + (int)accelerationCurrentValue);
             txt_prevAccel.setText("Prev = " + (int)accelerationPreviousValue);
-            txt_acceleration.setText("Acceleration = " + (int)changeInAccelleration);
+            txt_acceleration.setText("Acceleration = " + (int)changeInAcceleration);
 
-            prog_ShakeMeter.setProgress((int)changeInAccelleration);
+            prog_shakeMeter.setProgress((int) changeInAcceleration);
 
             // change colors based on amount of shaking
-            if (changeInAccelleration > 14){
+            if (changeInAcceleration > 14) {
                 txt_acceleration.setBackgroundColor(Color.RED);
             }
-            else if (changeInAccelleration > 5){
+            else if (changeInAcceleration > 5) {
                 txt_acceleration.setBackgroundColor(Color.parseColor("#fcad03"));
             }
-            else if (changeInAccelleration > 2) {
+            else if (changeInAcceleration > 2) {
                 txt_acceleration.setBackgroundColor(Color.YELLOW);
             }
             else {
                 txt_acceleration.setBackgroundColor(getResources().getColor(com.google.android.material.R.color.design_default_color_background));
             }
-
-            //update the graph
-            pointsPlotted++;
-
-            if (pointsPlotted > 1000){
-                pointsPlotted = 1;
-                series.resetData(new DataPoint[] { new DataPoint(1, 0)});
-            }
-            series.appendData(new DataPoint(pointsPlotted, changeInAccelleration), true,pointsPlotted);
-            viewport.setMaxX(pointsPlotted);
-            viewport.setMinX(pointsPlotted - 200);
         }
+
         @Override
         public void onAccuracyChanged(Sensor sensor, int i) {
 
@@ -97,35 +65,19 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_screen);
 
 
+        txt_acceleration = findViewById(R.id.txt_accel);
         txt_currentAccel = findViewById(R.id.txt_currentAccel);
         txt_prevAccel = findViewById(R.id.txt_prevAccel);
-        txt_acceleration = findViewById(R.id.txt_accel);
 
-        prog_ShakeMeter = findViewById(R.id.prog_ShakeMeter);
+        prog_shakeMeter = findViewById(R.id.prog_ShakeMeter);
 
-        //just a button
-//        B1 = findViewById(R.id.B1);
-//        B1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(MainActivity.this, Main2.class));
-//            }
-//        });
-
-        //initialze sensor object
+        // Initialize sensor object
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-
-        //sample graph code
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        viewport = graph.getViewport();
-        viewport.setScalable(true);
-        viewport.setYAxisBoundsManual(true);
-        graph.addSeries(series);
     }
 
     protected void onResume() {
@@ -138,4 +90,3 @@ public class Home extends AppCompatActivity {
         mSensorManager.unregisterListener(sensorEventListener);
     }
 }
-
